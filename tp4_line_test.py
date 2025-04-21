1073a3ab90c94f01559ded40d72f1827a4e9c377
Salvador Sanchez Pujol <ssanchezpujol@mail.austral.edu.ar>
2025-04-20 22:35:46 -0300

    Update tp4_line_test.py

--- tp4_line_test.py
+++ tp4_line_test.py
@@ -1,30 +1,46 @@
-def line():
-    CoA = float(input("Ingrese el coeficiente A:"))
-    CoB = float (input ("Ingrese el coeficiente B:"))
-    CoX1 = float (input ("Ingrese el coeficiente X1:"))
-    CoX2 = float (input ("Ingrese el coeficiente X2:"))
-    Y1 = CoA * CoX1 + CoB
-    Y2 = CoA * CoX2 + CoB
-    Distancia = ((CoX2 - CoX1)**2 + (Y2 - Y1)**2)**0.5
+import io
+import unittest.mock
+import line as ex2
 
-    print (f"El coeficiente A de su ecuación de la recta es: {CoA}")
-    print (f"El coeficiente B de su ecuación de la recta es: {CoB}")
-    print (f"El coeficiente X1 de su ecuación de la recta es: {CoX1}")
-    print (f"El coeficiente X2 de su ecuación de la recta es: {CoX2}")
 
-    print ("")
+class TP4LineTest(unittest.TestCase):
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_slice_advanced(self, mock_stdout):
+        inputs = iter(["2.3", "-4", "50", "-32.9"])
+        with unittest.mock.patch('builtins.input', side_effect=lambda y: next(inputs)):
+            ex2.line()
+            results = mock_stdout.getvalue().splitlines()
+            self.assertEqual(results[0], "El coeficiente A de su ecuación de la recta es: 2.3")
+            self.assertEqual(results[1], "El coeficiente B de su ecuación de la recta es: -4.0")
+            self.assertEqual(results[2], "El coeficiente X1 de su ecuación de la recta es: 50.0")
+            self.assertEqual(results[3], "El coeficiente X2 de su ecuación de la recta es: -32.9")
+            self.assertEqual(results[4], "")
+            self.assertEqual(results[5], "Para la siguiente ecuación:")
+            self.assertEqual(results[6], "\tY = 2.3X + -4.0")
+            self.assertEqual(results[7], "")
+            self.assertEqual(results[8], "Dados los siguientes puntos:")
+            self.assertEqual(results[9], "\tP1 (50.0, 110.99999999999999)")
+            self.assertEqual(results[10], "\tP2 (-32.9, -79.66999999999999)")
+            self.assertEqual(results[11], "")
+            self.assertEqual(results[12], "La distancia entre ellos es: 207.9121422620622")
+        inputs2 = iter(["1", "2", "50", "50"])
+        with unittest.mock.patch('builtins.input', side_effect=lambda y: next(inputs2)):
+            ex2.line()
+            results = mock_stdout.getvalue().splitlines()
+            self.assertEqual(results[13], "El coeficiente A de su ecuación de la recta es: 1.0")
+            self.assertEqual(results[14], "El coeficiente B de su ecuación de la recta es: 2.0")
+            self.assertEqual(results[15], "El coeficiente X1 de su ecuación de la recta es: 50.0")
+            self.assertEqual(results[16], "El coeficiente X2 de su ecuación de la recta es: 50.0")
+            self.assertEqual(results[17], "")
+            self.assertEqual(results[18], "Para la siguiente ecuación:")
+            self.assertEqual(results[19], "\tY = 1.0X + 2.0")
+            self.assertEqual(results[20], "")
+            self.assertEqual(results[21], "Dados los siguientes puntos:")
+            self.assertEqual(results[22], "\tP1 (50.0, 52.0)")
+            self.assertEqual(results[23], "\tP2 (50.0, 52.0)")
+            self.assertEqual(results[24], "")
+            self.assertEqual(results[25], "La distancia entre ellos es: 0.0")
 
-    print ("Para la siguiente ecuación:")
-    print (f"\tY = {CoA}X + {CoB}")
 
-    print ("")
-
-    print ("Dados los siguientes puntos:")
-    print (f"\tP1 ({CoX1}, {Y1})")
-    print (f"\tP2 ({CoX2}, {Y2})")
-
-    print ()
-
-    print (f"La distancia entre ellos es: {Distancia}")
-
-line()
+if __name__ == '__main__':
+    unittest.main()
