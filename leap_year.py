9368ec2028b3e56b1433470e352a40f9425a2925
Salvador Sanchez Pujol <ssanchezpujol@mail.austral.edu.ar>
2025-06-14 20:33:09 -0300

    leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,25 +1,12 @@
-import math
+def es_bisiesto(anio):
+    return (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0)
 
 def main():
-    A = float(input("Ingrese el coeficiente A: "))
-    B = float(input("Ingrese el coeficiente B: "))
-    X1 = float(input("Ingrese el coeficiente X1: "))
-    X2 = float(input("Ingrese el coeficiente X2: "))
-
-    Y1 = A * X1 + B
-    Y2 = A * X2 + B
-
-    print(f"\nEl coeficiente A de su ecuación de la recta es: {A}")
-    print(f"El coeficiente B de su ecuación de la recta es: {B}")
-    print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
-    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
-
-    print(f"\nPara la siguiente ecuación:\n\tY = {A}X + {B}")
-
-    print(f"\nDados los siguientes puntos:\n\tP1 ({X1}, {Y1})\n\tP2 ({X2}, {Y2})")
-
-    distancia = math.sqrt((X2 - X1)**2 + (Y2 - Y1)**2)
-    print(f"\nLa distancia entre ellos es: {distancia}")
+    anio = int(input("Ingrese un año: "))
+    if es_bisiesto(anio):
+        print(f"El año {anio} es bisiesto")
+    else:
+        print(f"El año {anio} no es bisiesto")
 
 if __name__ == "__main__":
     main()
