94bbd53229ef4fb49876675073b2b2d812467db1
Salvador Sanchez Pujol <ssanchezpujol@mail.austral.edu.ar>
2025-06-14 20:36:41 -0300

    leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,12 +1,6 @@
-def es_bisiesto(anio):
-    return (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0)
-
-def main():
-    anio = int(input("Ingrese un año: "))
-    if es_bisiesto(anio):
-        print(f"El año {anio} es bisiesto")
+def leap_year():
+    year = int(input("Ingrese un año: "))
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"El año {year} es bisiesto")
     else:
-        print(f"El año {anio} no es bisiesto")
-
-if __name__ == "__main__":
-    main()
+        print(f"El año {year} no es bisiesto")
